6ae23e9f17e6689d1e6d4dd961a1c58555aaee44
Antipov Artyom <temansky@yandex.ru>
2025-03-08 21:14:51 +0300

    Add Schema to TeacherCreateDto

--- src/main/java/com/example/service/student/StudentSubject.java
+++
@@ -1,6 +0,0 @@
-package com.example.service.student;
-
-public record StudentSubject(
-        String name
-) {
-}
--- src/main/java/com/example/service/teacher/TeacherCreateDto.java
+++ src/main/java/com/example/service/teacher/TeacherCreateDto.java
@@ -3,11 +3,15 @@ package com.example.service.teacher;
 import java.time.LocalTime;
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record TeacherCreateDto(
         String name,
         String surname,
         String patronymic,
+        @Schema(type = "String", pattern = "HH:mm:SS")
         LocalTime timeStart,
+        @Schema(type = "String", pattern = "HH:mm:SS")
         LocalTime timeEnd,
         List<TeacherSubject> subjects
 ) {
