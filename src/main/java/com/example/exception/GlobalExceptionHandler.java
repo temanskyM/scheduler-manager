6fabaad755713686abe1d04429f874a0c0ceb0df
Antipov Artyom <temansky@yandex.ru>
2025-03-08 21:38:12 +0300

    Fix swagger ui

--- src/main/java/com/example/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/exception/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,17 +16,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Hidden
 public class GlobalExceptionHandler {
 
-    //    @ExceptionHandler(MethodArgumentNotValidException.class)
-    //    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    //    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-    //        Map<String, String> errors = new HashMap<>();
-    //        ex.getBindingResult().getAllErrors().forEach(error -> {
-    //            String fieldName = ((FieldError) error).getField();
-    //            String errorMessage = error.getDefaultMessage();
-    //            errors.put(fieldName, errorMessage);
-    //        });
-    //        return ResponseEntity.badRequest().body(errors);
-    //    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
 
     @ExceptionHandler(ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
